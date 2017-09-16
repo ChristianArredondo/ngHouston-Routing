e8daaffd91079b8b76f9c6b6a0e750a4bed7112a
christianarredondo <christianarredondo@utexas.edu>
2017-09-16 14:48:32 -0500

    set up toolbar and remove spacing styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Section1Component } from './section1/section1.component';
 
+import { RouterModule} from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MdToolbarModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent,
     Section1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
